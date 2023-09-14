4723c8971c9edc1bd705eb5b4c2d1d4b19b9ee33
Wasif Nazeer <wasifnazeer11@gmail.com>
2023-09-15 02:29:02 +0500

    first one

--- bin/step01_hello_lambda.ts
+++ bin/step01_hello_lambda.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from '@aws-cdk/core';
 import { Step01HelloLambdaStack } from '../lib/step01_hello_lambda-stack';
+import { Construct } from 'constructs';
+
 
 const app = new cdk.App();
 new Step01HelloLambdaStack(app, 'Step01HelloLambdaStack', {
